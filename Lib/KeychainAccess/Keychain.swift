c0513e0784ff87738c31a5695e6732b24074fd36
Alexander Tovstonozhenko <a.tovstonozhenko@mozidev.com>
2019-01-17 17:26:48 +0200

    Fix access groups query handling

--- Lib/KeychainAccess/Keychain.swift
+++ Lib/KeychainAccess/Keychain.swift
@@ -1193,13 +1193,11 @@ extension Options {
 
         switch itemClass {
         case .genericPassword:
-            query[AttributeService] = service
-            // Access group is not supported on any simulators.
-            #if (!arch(i386) && !arch(x86_64)) || (!os(iOS) && !os(watchOS) && !os(tvOS))
             if let accessGroup = self.accessGroup {
                 query[AttributeAccessGroup] = accessGroup
+            } else {
+                query[AttributeService] = service
             }
-            #endif
         case .internetPassword:
             query[AttributeServer] = server.host
             query[AttributePort] = server.port
